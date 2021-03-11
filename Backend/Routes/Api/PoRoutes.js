b8690b4b7291089cc3562b1f3d5c6af87b229895
bgolden1 <brodygolden7@gmail.com>
2021-03-11 11:47:09 -0500

    small changes to PO system

--- Backend/Routes/Api/PoRoutes.js
+++ Backend/Routes/Api/PoRoutes.js
@@ -5,5 +5,10 @@ let router = express.Router();
 
 router.get("/po", POController.getAll);
 router.get("/po:name", POController.getByName);
+router.post("/po", (req, res) => {
+    //validate input
+    //create new po
+    //save it and set response
+});
 
 module.exports = router;
--- Backend/Validation/Orders.js
+++ Backend/Validation/Orders.js
@@ -1,6 +1,6 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
-module.exports = function validateRegisterInput(data) {
+module.exports = function validatePOInput(data) {
     let errors = {};
 
   // Convert empty fields to an empty string so we can use validator functions
