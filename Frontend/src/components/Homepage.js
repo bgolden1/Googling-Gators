91ecb4c3761aa2fc4d0ce6058ff6c034217de2ae
edwardzhang5 <edwardzhang5@gmail.com>
2021-03-16 16:12:52 -0400

    made login page using template from tutorial that brody sent, added a second button option on front page to match style

--- Frontend/src/components/Homepage.js
+++ Frontend/src/components/Homepage.js
@@ -3,15 +3,17 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import logo from './gatorlooplogo.jpg'
+import Navbar from "./layout/Navbar";
 
 class Homepage extends Component {
   render() {
     return (
-      <div>
-        <header className='App-header'>
-          <img src={logo} className='App-logo'/>
-          <h1>Welcome to the GatorLoop Inventory Management System!</h1>
-        </header>
+        <div>
+            <Navbar/>
+            <header className='App-header'>
+                <img src={logo} className='App-logo'/>
+                <h1>Welcome to the GatorLoop Inventory Management System!</h1>
+            </header>
         <>
           <div class="text-center">
             <Link to="/login">
@@ -19,7 +21,23 @@ class Homepage extends Component {
                 variant='outline-secondary'
                 size='lg'
                 className='App-button'>
-                Login</Button>{' '}
+                            <b>Login</b></Button>{' '}
+
+            // This is the other button style
+            <Button
+                 style={{
+                             width: "150px",
+                             borderRadius: "3px",
+                             letterSpacing: "1.5px",
+                             marginTop: "1rem"
+                        }}
+                        type="submit"
+                        className="btn waves-effect waves-light"
+                  >
+                        <b>Login</b>
+                        </Button>
+            // Delete this section if you don't like the button
+
             </Link>
 
             <Link to="/register">
--- Frontend/src/components/Login_Page.js
+++ Frontend/src/components/Login_Page.js
@@ -1,18 +1,82 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class Login_Page extends Component {
-    handleClick() {
-        this.props.history.push('/')
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            errors: {}
+        };
     }
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+    };
+    onSubmit = e => {
+        e.preventDefault();
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        console.log(userData);
+    };
     render() {
+        const { errors } = this.state;
         return (
-            <div>
-                <header className='App-login'>
-                    <h>This site does absoultely nothing right now</h>
-                </header>
+            <div className="container">
+                <div style={{ marginTop: "4rem" }} className="row">
+                    <div className="col s8 offset-s2">
+                        <Link to="/" className="btn-flat waves-effect">
+                            <i className="material-icons left">keyboard_backspace</i> Back to
+              home
+            </Link>
+                        <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                            <h4><b>Welcome</b> Gatorloop Members</h4>
+                            <p className="grey-text text-darken-1">
+                                Don't have an account? <Link to="/register">Register</Link>
+                            </p>
+                        </div>
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.email}
+                                    error={errors.email}
+                                    id="email"
+                                    type="email"
+                                />
+                                <label htmlFor="email">UFL Email</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.password}
+                                    error={errors.password}
+                                    id="password"
+                                    type="password"
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                                <button
+                                    style={{
+                                        width: "150px",
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }}
+                                    type="submit"
+                                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                                   >
+                                    <b>Login</b>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
