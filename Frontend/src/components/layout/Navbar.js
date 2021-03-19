dc55db1d9f6fdcf9cb4cbe4a0a5b2039107db186
edwardzhang5 <edwardzhang5@gmail.com>
2021-03-19 12:30:37 -0400

    Revert "fixed nav bar"
    This reverts commit 8c88578d1afd5823f39d7e3ef36e1e118442cd61.

--- Frontend/src/components/layout/Navbar.js
+++ Frontend/src/components/layout/Navbar.js
@@ -1,50 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import logo from '../gatorlooplogo.jpg'
-
 class Navbar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">
-                        <img src={logo} alt="" width="60" height="30" class="d-inline-block "></img>
-                             Gatorloop
-                    </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr class="dropdown-divider"></hr></li>
-                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                                <li class="nav-item">
-                                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                                </li>
-                          </ul>
-                            <form class="d-flex">
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                                    <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+            <div className="navbar-fixed">
+                <nav className="z-depth-0">
+                    <div className="nav-wrapper white">
+                        <Link
+                            to="/"
+                            style={{
+                                fontFamily: "monospace"
+                            }}
+                            className="col s5 brand-logo center black-text"
+                        >
+                            <i className="material-icons">code</i>
+              Menu
+            </Link>
+                    </div>
+                </nav>
+            </div>
         );
     }
 }
