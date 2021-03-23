ee085e74794f47db671edfb30a6111840d80697a
nicolenliang <nicolenliang@gmail.com>
2021-03-23 05:39:07 -0400

    rough header menu bar

---
+++ Frontend/src/components/layout/Menubar.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Link, Router } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+class Menubar extends Component {
+    render() {
+        return (
+            <Navbar variant="light" bg="light">
+                <div >
+                    <Navbar.Brand href="/">GatorLoop</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link>
+                            <Link
+                                to="/dashboard"
+                                style={{fontFamily: "montserrat"}}
+                                className="s5 brand-logo center black-text">
+                                Dashboard
+                            </Link>
+                        </Nav.Link>
+
+                        <Nav.Link>
+                            <Link 
+                                to="/inventory_page"
+                                style={{fontFamily: "montserrat"}}
+                                >
+                                Inventory
+                            </Link>
+                        </Nav.Link>
+
+                        <Nav.Link>
+                            <Link 
+                                to="/order"
+                                style={{fontFamily: "montserrat"}}
+                                className="col s5 brand-logo center black-text">
+                                Order History
+                            </Link>
+                        </Nav.Link>
+
+                    </Nav>
+                </div>             
+            </Navbar>
+        );
+    }
+}
+
+export default Menubar;
