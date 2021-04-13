a676e69da28453acc87e98f5893a8fe81b986d6f
bgolden1 <brodygolden7@gmail.com>
2021-04-13 15:27:03 -0400

    User Mangement navigation for admins

--- Frontend/src/components/layout/Menubar.js
+++ Frontend/src/components/layout/Menubar.js
@@ -3,10 +3,34 @@ import { Link, Router } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import logo from "./gatorlooplogo.png";
+import jwt_decode from "jwt-decode";
 
 
 
 class Menubar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user_name: "",
+            user_email: "",
+            user_role: "",
+            logged_in: false
+        }
+    }
+
+    componentDidMount() {
+        try {
+            const token = global.localStorage.getItem("jwtToken");
+            const decoded = jwt_decode(token);
+            this.setState({ user_name: decoded.name, user_email: decoded.email, user_role: decoded.role, logged_in: true });
+            console.log(decoded);
+        }
+        catch (err) {
+            console.log(err);
+        }
+
+    }
+
     render() {
         return (
             <Navbar variant="light" bg="light">
@@ -36,6 +60,10 @@ class Menubar extends Component {
                                         
                                     </ul>
                                 </li>
+
+                                {this.state.user_role == "admin" && <li class="nav-item">
+                                    <a class="nav-link " id="users-tab"  href="/users" role="tab" aria-controls="users" aria-selected="false">Manage Users</a>
+                                </li>}
 
                                 <li class="nav-item">
                                     <a class="nav-link " id="settings-tab" href="/settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
