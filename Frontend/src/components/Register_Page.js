14ed6c68cac1245640317a40eb31b485b31eea4e
bgolden1 <brodygolden7@gmail.com>
2022-01-18 09:56:46 -0500

    register fix

--- Frontend/src/components/Register_Page.js
+++ Frontend/src/components/Register_Page.js
@@ -36,7 +36,7 @@ class Register_Page extends Component {
         axios.post("https://gatorloop-ims.herokuapp.com/api/register", newUser).then(res => {
             this.setState({completed: true})
         }).catch(err => {
-            alert(String(err.response.data));
+            alert(JSON.stringify(err.response.data));
             this.setState({errors: err.response.data});
         })
     };
